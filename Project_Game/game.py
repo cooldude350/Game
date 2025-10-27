32a8b33512821f60eb8414cc584088f4d9fbbb1c
yojithvemula <yojithvemula@gmail.com>
2025-10-27 19:48:07 +0530

    Set up Pygame with a green background window
    Initialize Pygame and create a basic game window.

--- Project_Game/game.py
+++ Project_Game/game.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import pygame
+
+
+pygame.init()
+pygame.display.set_caption("Test")
+screen = pygame.display.set_mode((800, 600))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 255, 0))
+    pygame.display.update()       
+
